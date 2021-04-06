961f9af8d617cc5d1b5cc8f5fa9e09498fb4a2b1
Kirill Neskoromny <kneskoromny@gmail.com>
2021-04-06 09:56:26 +0300

    found most common type of animal

--- PersonalQuiz/ViewControllers/ResultsViewController.swift
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -10,20 +10,51 @@ import UIKit
 class ResultsViewController: UIViewController {
     
     // 1. Передать сюда массив с ответами +
-    // 2. Определить наиболее часто встречающийся тип животного
+    // 2. Определить наиболее часто встречающийся тип животного +
     // 3. Отобразить результат в соответсвии с этим животным
     // 4. Избавиться от кнопки возврата назад на экране результатов
     
     var answers: [Answer]!
+    
+    private var result = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for answer in answers {
-            print(answer.type)
-        }
-
+        answerTypeCounter()
+   
+    }
+    
+    private func answerTypeCounter() {
         
+        
+        var animalCounter = ["dog": 0, "cat": 0, "rabbit": 0, "turtle": 0]
+        var dog = 0, cat = 0, rabbit = 0, turtle = 0
+        
+        for answer in answers {
+            switch answer.type {
+            case .dog:
+                dog += 1
+                animalCounter.updateValue(dog, forKey: "dog")
+            case .cat:
+                cat += 1
+                animalCounter.updateValue(cat, forKey: "cat")
+            case .rabbit:
+                rabbit += 1
+                animalCounter.updateValue(rabbit, forKey: "rabbit")
+            default:
+                turtle += 1
+                animalCounter.updateValue(turtle, forKey: "turtle")
+            }
+        }
+        
+        let maximumValue = animalCounter.values.sorted().last!
+        
+        for (animal, count) in animalCounter {
+            if count == maximumValue {
+                result = animal
+            }
+        }
     }
 
 }
